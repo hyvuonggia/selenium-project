e46caf32a33c36ab599c80d26c16c96245a1cea0
Vuong Gia Hy <hyvuonggia@gmail.com>
2021-11-27 09:58:42 +0100

    Added main class

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+
+os.system('behave .\\features\\sign_up.feature')
+os.system('behave .\\features\\sign_in.feature')
+os.system('behave .\\features\\contact_us.feature')
+os.system('behave .\\features\\search.feature')
+os.system('behave .\\features\\newsletter.feature')
+
+input()
